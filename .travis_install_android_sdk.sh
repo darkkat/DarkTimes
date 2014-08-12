98741f82e9ba606d8cd791c750ab506970895377
DarkMentat <indraua@gmail.com>
2014-08-13 01:11:05 +0300

    Update .travis_install_android_sdk.sh

--- .travis_install_android_sdk.sh
+++ .travis_install_android_sdk.sh
@@ -2,11 +2,8 @@
 
 # Script installs Android SDK components
 
-<<<<<<< HEAD
 spawn android update sdk --filter tools,platform-tools,build-tools-19.0.0,extra-android-support,android-17,sysimg-17,addon-google_apis-google-17,android-19,sysimg-19,addon-google_apis-google-19,addon-google_apis-google-19,extra-google-play_billing,extra-google-m2repository,extra-google-analytics_sdk_v2,extra-google-gcm,extra-google-google_play_services,extra-google-play_apk_expansion,extra-android-m2repository --no-ui --force --all
-=======
-spawn android update sdk --filter tools,platform-tools,build-tools-19.0.0,extra-android-support,android-17,sysimg-17,addon-google_apis-google-17,android-20,sysimg-20,addon-google_apis-google-20,addon-google_apis-google-20,extra-google-play_billing,extra-google-m2repository,extra-google-analytics_sdk_v2,extra-google-gcm,extra-google-google_play_services,extra-google-play_apk_expansion,extra-android-m2repository --no-ui --force --all
->>>>>>> origin/master
+
 expect "Do you accept the license *:"
 send -- "y\r"
 expect "Do you accept the license *:"
